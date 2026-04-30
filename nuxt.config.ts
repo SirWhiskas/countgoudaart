a82f422ae03fb3051ea02670ce3d1635fb1b2087
Aaron Pillert <aaron.pillert@inuvo.com>
2026-04-30 10:06:41 -0500

    Adding vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/image',
+    '@vercel/analytics',
   ],
 
   css: ['~/assets/css/main.css', 'primeicons/primeicons.css'],
